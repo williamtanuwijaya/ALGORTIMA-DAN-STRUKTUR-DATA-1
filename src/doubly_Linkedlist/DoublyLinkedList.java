a26570343c2e13a4d147ac82549d1a775804eee7
williamtanuwijaya <will.tw0807@gmail.com>
2023-05-05 08:34:48 +0700

    update

--- src/doubly_Linkedlist/DoublyLinkedList.java
+++ src/doubly_Linkedlist/DoublyLinkedList.java
@@ -4,6 +4,40 @@ class Node{
     int data;
     Node prev;
     Node next;
+    public Node(){
+        data = 0;
+        prev = null;
+        next = null;
+    }
+    public Node(int data, Node prev, Node next) {
+        this.data = data;
+        this.prev = prev;
+        this.next = next;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Node getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
 public class DoublyLinkedList {
     public static void main(String[] args) {
