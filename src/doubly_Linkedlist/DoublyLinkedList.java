4b9d9c85ab8e24f1aa31e77708a0c09c42cda226
williamtanuwijaya <will.tw0807@gmail.com>
2023-05-05 08:31:54 +0700

    update

---
+++ src/doubly_Linkedlist/DoublyLinkedList.java
@@ -0,0 +1,12 @@
+package doubly_Linkedlist;
+
+class Node{
+    int data;
+    Node prev;
+    Node next;
+}
+public class DoublyLinkedList {
+    public static void main(String[] args) {
+
+    }
+}
